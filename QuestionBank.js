01b50b50bd82ffa2b47aa86873dcf3eb29b743d8
bhavaniananth <95908151+bhavaniananth@users.noreply.github.com>
2021-12-20 22:19:23 -0800

    Add files via upload

---
+++ QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt : "What is 1+3?",
+        OptionA : "24",
+        OptionB : "21",
+        OptionC : "4",
+        OptionD : "2" ,
+        answer : "C",
+    },
+    {
+    prompt : "Which is a Streaming Platform?",
+    OptionA : "Twitter ",
+    OptionB : "Twitch",
+    OptionC : "Google",
+    OptionD : "Robinhood" ,
+    answer : "B",
+    },
+    {
+    prompt : "Best tech youtube channel?",
+    OptionA : "PedroTech",
+    OptionB : "Dev Ed",
+    OptionC : "Fireship",
+    OptionD : "TechWithTim",
+    answer : "A",
+    },
+];
